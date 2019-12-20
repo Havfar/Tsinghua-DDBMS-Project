8fd5b72199c248b44f8249683a9df1f9c32dd86b
Tobias Skjelvik <trampcha@gmail.com>
2019-12-20 19:58:55 +0800

    added callback for search

--- dbms-client/src/components/ClientSite.js
+++ dbms-client/src/components/ClientSite.js
@@ -2,19 +2,88 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Article from './Article';
+import User from './User';
 import SearchBar from './Searchbar';
 
 
 export default class ClientSite extends React.Component {
     constructor (props) {
         super(props)
-        this.state = { visible: true }  
+        this.state = { 
+        visible: true,
+        SAid: undefined,
+        SUid: undefined,
+        TypeSearch: undefined
+        }  
+    }
+
+    callBackSearch = (data) => {
+        switch(data.TypeSearch){
+            case "article":
+                this.setState({
+                    SAid: data.SAid,
+                    SUid: data.SUid,
+                    TypeSearch: data.TypeSearch
+                }
+                )
+                break;
+            case "user":
+                this.setState({
+                    SUid: data.SUid,
+                    TypeSearch: data.TypeSearch
+                })
+                break;
+            case "allArticles":
+                this.setState({
+                    TypeSearch: data.TypeSearch
+                })
+                break;
+            case "allUsers":
+                this.setState({
+                    TypeSearch: data.TypeSearch
+                })
+                break;
+            default:
+                break;
+        }
     }
 
     render () {
-    return <div>
-        <SearchBar/>
-        <Article></Article>
-    </div>
+        let TypeSearch = this.state.TypeSearch;
+        if(TypeSearch == "article"){
+            return  <div>
+                        <SearchBar callBackSearch={this.callBackSearch}/>
+                        <Article aid={this.state.SAid} uid={this.state.SUid}></Article>
+                        <User></User>
+                    </div>
+        }
+        else if (TypeSearch == "user"){
+            return  <div>
+                        <SearchBar callBackSearch={this.callBackSearch}/>
+                        <Article></Article>
+                        <User uid={this.state.SUid}></User>
+                    </div>
+        }
+        else if (TypeSearch == "allArticles"){
+            return  <div>
+                        <SearchBar callBackSearch={this.callBackSearch}/>
+                        <Article></Article>
+                        <User></User>
+                    </div>
+        }
+        else if (TypeSearch == "allUsers"){
+            return  <div>
+                        <SearchBar callBackSearch={this.callBackSearch}/>
+                        <Article></Article>
+                        <User></User>
+                    </div>
+        }
+        else{
+            return  <div>
+                        <SearchBar callBackSearch={this.callBackSearch}/>
+                        <Article></Article>
+                        <User></User>
+                    </div>
+        }
     }
 }
